04e86dce4cf6687f2caa8acbd9a85a8de1fed4d4
HiIAmKarthiK <68651191+HiIAmKarthiK@users.noreply.github.com>
2020-07-31 19:25:10 +0530

    js files

---
+++ index.js
@@ -0,0 +1,119 @@
+
+//detects keyboard button press
+
+
+document.addEventListener("keypress", function (event) {
+
+  var keyboardButton = event.key;
+
+
+
+  makesound(keyboardButton);
+
+  createAnimation(keyboardButton);
+
+
+
+
+
+
+});
+
+
+//detects mouse button press
+
+var buttonLimit = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i <= buttonLimit; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+
+    makesound(buttonInnerHTML);
+
+    createAnimation(buttonInnerHTML);
+
+
+
+
+
+  });
+}
+
+
+
+
+//common function to execute in both cases
+
+
+function makesound(key) {
+
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+
+      break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+
+      break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+
+      break;
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+
+      break;
+
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+
+      break;
+
+    default:
+      console.log(buttonInnerHTML);
+      break;
+
+  }
+
+
+
+}
+
+//Animation code
+
+function createAnimation(pressedKey) {
+
+  var activeKey = document.querySelector("." + pressedKey);
+  activeKey.classList.add("pressed");
+  setTimeout(function () {
+    activeKey.classList.remove("pressed");
+  }, 100)
+
+
+
+}
+
+
+
